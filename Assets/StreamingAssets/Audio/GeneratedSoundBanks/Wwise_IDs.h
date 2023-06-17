ab63d81da5b1a45d04665d8d1015b2fb0a50db6a
jonasbengio <106099235+jonasbengio@users.noreply.github.com>
2023-06-17 23:10:18 +0200

    add soundbank

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,25 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID BLOB_PLAYER = 3768464496U;
         static const AkUniqueID COMEON = 2931178234U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace PLAYERMOVING
+        {
+            static const AkUniqueID GROUP = 545959236U;
+
+            namespace STATE
+            {
+                static const AkUniqueID MOVING = 2649703675U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID NOT_MOVING = 3053284133U;
+            } // namespace STATE
+        } // namespace PLAYERMOVING
+
+    } // namespace STATES
 
     namespace BANKS
     {
