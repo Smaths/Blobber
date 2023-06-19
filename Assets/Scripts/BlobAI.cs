e416b7b514a8653877cc72fe5b58ab7847c829ad
Eric Wroblewski <ericwroble@gmail.com>
2023-06-19 18:59:29 -0400

    Implement NavMeshAgent on bad blobs

--- Assets/Scripts/BlobAI.cs
+++ Assets/Scripts/BlobAI.cs
@@ -1,12 +1,50 @@
+using System.Collections;
 using DG.Tweening;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BlobAI : MonoBehaviour
 {
     [SerializeField] private int _pointValue = 5;
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+    [SerializeField] private GameObject _playerBlob;
+    [Tooltip("How far the blob can see.")]
+    [SuffixLabel("seconds")]
+    [SerializeField] private float _searchRate = 0.25f;
+    [SerializeField] private float _searchRadius;
+    [SerializeField] private LayerMask _searchMask;
 
     private bool _isDestroying;
 
+    #region Lifecycle
+    private void OnDrawGizmosSelected()
+    {
+        // Draw the search radius gizmo in the Unity Editor
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _searchRadius);
+    }
+
+    private void OnValidate()
+    {
+        _playerBlob ??= FindObjectOfType<PlayerController>().gameObject;
+    }
+
+    private void Awake()
+    {
+        _playerBlob ??= FindObjectOfType<PlayerController>().gameObject;
+    }
+
+    private void Start()
+    {
+        if (_navMeshAgent)
+        {
+            StartCoroutine(SearchForPlayerCoroutine());
+        }
+    }
+    #endregion
+
+    #region Collision
     private void OnCollisionEnter(Collision other)
     {
         print($"{gameObject.name} - {other.gameObject.name}");
@@ -15,7 +53,6 @@ public class BlobAI : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (_isDestroying) return;  // guard statement
-
 
         print(_pointValue > 0
             ? $"{gameObject.name} Collision - Add {_pointValue}"
@@ -30,5 +67,25 @@ public class BlobAI : MonoBehaviour
         sequence.OnStart(() => _isDestroying = true);
         sequence.OnComplete(() => Destroy(gameObject, 0.1f));
         sequence.Play();
+    }
+    #endregion
+
+    private IEnumerator SearchForPlayerCoroutine()
+    {
+        while (true)
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, _searchRadius, _searchMask);
+
+            foreach (Collider col in colliders)
+            {
+                // Perform actions on the found object(s)
+                print($"{gameObject.name} - Found object: {col.gameObject.name}");
+                _navMeshAgent.SetDestination(col.transform.position);
+            }
+
+            _navMeshAgent.SetDestination(_playerBlob.transform.position);
+
+            yield return new WaitForSeconds(_searchRate);
+        }
     }
 }
