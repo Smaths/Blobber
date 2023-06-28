5461585de69180ba7b10a5335cdfd6ae17078222
Eric Wroblewski <ericwroble@gmail.com>
2023-06-28 12:53:34 -0400

    Before upgrading to 2022.3.3f1

--- Assets/Scripts/UI/UI_Boost.cs
+++ Assets/Scripts/UI/UI_Boost.cs
@@ -42,7 +42,7 @@ namespace UI
             _image.color = _filledColor;
             _image.fillAmount = 100f;
 
-            //_image.transform.DOPunchScale(Vector3.one * 0.8f, 0.5f, 3, 0.5f);
+            _image.transform.DOPunchScale(Vector3.one * 0.8f, 0.5f, 3, 0.5f);
         }
     }
 }
